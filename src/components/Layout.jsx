9256cf14b74cbaf35e71c7adbbf24014f3e85320
estela <estelathomson@me.com>
2023-08-05 17:05:14 -0400

    init commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Products from "./Products"
+
+
+const Layout = () => {
+  return (
+    <main className="wrapper">
+      <h1>Marketplace</h1>
+      <Products />
+    </main>
+  )
+}
+
+export default Layout
